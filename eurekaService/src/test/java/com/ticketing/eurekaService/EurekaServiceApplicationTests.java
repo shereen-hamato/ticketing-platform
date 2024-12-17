0ca105e7d4a8ced438791936b685857e072586f3
Shereen Ahmed <shereen.hamato@gmail.com>
2024-12-17 11:57:32 +0800

    Add eureka init code

---
+++ eurekaService/src/test/java/com/ticketing/eurekaService/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketing.eurekaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
